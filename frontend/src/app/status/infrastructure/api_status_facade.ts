3696592bf5fed99bffedcece498cacfbb32ff127
Rodrigo Vasconcelos de Barros <4694457-rodrigopk@users.noreply.gitlab.com>
2021-05-22 14:47:01 +0200

    Use env variable for api url

--- frontend/src/app/status/infrastructure/api_status_facade.ts
+++ frontend/src/app/status/infrastructure/api_status_facade.ts
@@ -1,14 +1,16 @@
+import { EnvironmentService } from '../../../libs/config';
 import { HttpService, IHttpService } from '../../../libs/http';
 
 export class ApiStatusFacade {
-  private baseApiUrl = 'http://localhost:3000';
-
-  constructor(private readonly httpService: IHttpService = new HttpService()) {}
+  constructor(
+    private readonly httpService: IHttpService = new HttpService(),
+    private readonly configService = EnvironmentService,
+  ) {}
 
   public async getStatus(): Promise<{ status: string }> {
     try {
       const result = await this.httpService.get<{ status: string }>(
-        `${this.baseApiUrl}/health`,
+        `${this.configService.baseApiUrl()}/health`,
       );
 
       return { status: result.data.status };
--- frontend/src/libs/config/environment.service.ts
+++ frontend/src/libs/config/environment.service.ts
@@ -2,4 +2,8 @@ export class EnvironmentService {
   public static isReactQueryDevtoolsEnabled() {
     return process.env.REACT_APP_ENABLE_QUERY_DEVTOOLS === 'true';
   }
+
+  public static baseApiUrl() {
+    return process.env.REACT_APP_BASE_API_URL;
+  }
 }
